2400a13733131ee878f29675c3079b08742f5f0b
reannu123 <reannumon123@gmail.com>
2023-03-18 12:15:35 +0800

    Updated to 16.0.0 and fixed links for patches

--- atmos_downloader.py
+++ atmos_downloader.py
@@ -79,23 +79,15 @@ print("Downloading Patches...")
 isDlPatches = True
 if isDlPatches:
     # Patches downloader
-    url = 'https://api.github.com/repos/ITotalJustice/patches/releases/latest'
+    url = 'https://sigmapatches.coomer.party/sigpatches.zip'
+    
     response = requests.get(url)
-
-    # Raise an exception if the API call fails.
-    response.raise_for_status()
-
-    data = response.json()
-    for asset in data['assets']:
-        if asset['name'].endswith('.zip'):
-            dl_file(asset['name'], asset['browser_download_url'])
-            print(asset['browser_download_url'])
-            zipfile.ZipFile(asset['name']).extractall("download")
-
-            # Delete the zip file
-            os.remove(asset['name'])
-            print()
-            break
+    dl_file('sigpatches.zip', url)
+    print(url)
+    zipfile.ZipFile('sigpatches.zip').extractall("download")
+    # Delete the zip file
+    os.remove("sigpatches.zip")
+    print()
 
 
 
@@ -108,10 +100,11 @@ if isDlFirmware == "y":
     version = input()
     # if version is empty, use the latest version
     if version == "":
-        version = "14.1.2"
+        version = "16.0.0"
     print("Downloading firmware " + version)
     url = f"https://archive.org/download/nintendo-switch-global-firmwares/Firmware%20{version}.zip"
     file = "firmware.zip"
+    print(url)
     if os.path.exists('download/firmware'):
         # Delete the firmware folder
         os.rmdir('download/firmware')
